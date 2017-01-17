adcc85b052e57e17626cf7a9e65ce7cc1f599dbb
a.gavrilov <a.gavrilov@TAIPOVAO>
2017-01-17 16:14:14 +0300

    clean up code

--- WpfSample/MainWindow.xaml.cs
+++ WpfSample/MainWindow.xaml.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WmTouchDevice;
 
 namespace WpfSample
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
